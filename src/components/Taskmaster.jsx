4aa8d9d0cfcc1178fad867ddef0b59d20c4528b7
DylanVallejo <dylan1811vallejo@gmail.com>
2023-01-04 21:06:20 -0500

    adding the form for the task a method for create a task and every task is an object that containts the input value and a boolean

--- src/components/Taskmaster.jsx
+++ src/components/Taskmaster.jsx
@@ -3,10 +3,38 @@ import styles from './Taskmaster.module.css'
 const Taskmaster =() =>{
     const [input, setInput] = useState('');
     const [task, setTask] = useState([])
+    
+    
+    // creando el task en un objeto con el texto y un valor booleano
+    //  unciamente si el input no es un string vacio ademas de limpiar el texto del input 
+    const add = ( e ) =>{
+        e.preventDefault();
+        if (input === '') return;
+        setTask([...task,{
+            text:input,
+            completed:false
+        }]);
+        setInput('');
+    }
+    
+    console.log(task);
 
     
     return(
-        <h1 className={styles.heading}>TaskMaster</h1>
+        
+        <div className='container'>
+            <h1 className={styles.heading}>TaskMaster</h1>
+                {/* creando un formulario para obtener el valor del nuevo task*/}
+                <form onSubmit={add}>
+                    <input
+                        placeholder='New Task?'
+                        className='in-text'
+                        onChange={e => setInput(e.target.value)}
+                        value={input}
+                        />
+                    <button className='add-btn'>Add</button>
+                </form>
+        </div>
         
     )
 }
